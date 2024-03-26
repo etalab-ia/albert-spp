670e32a67ef2dd0409d210adc584ee9966b0addd
leoguillaume <leo.guillaume@modernisation.gouv.fr>
2024-03-26 16:04:22 +0100

    feat: simplify deployment

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,16 @@
 FROM python:3.10-slim
 
-ENV PYTHONPATH="$PYTHONPATH:/code/api"
-
 RUN addgroup --gid 1100 albert && \
     adduser --uid 1001 --gid 1100 --home /code albert
 
 WORKDIR /code
-ADD . api
-RUN pip install --no-cache-dir --upgrade -r ./api/requirements.txt
+
+# add requirements.txt before the rest of the code to cache the pip install
+ADD ./requirements.txt /code/api/requirements.txt
+RUN pip install --no-cache-dir -r /code/api/requirements.txt
+
+# add the rest of the code
+ENV PYTHONPATH="$PYTHONPATH:/code/api"
+ADD . /code/api
 
 USER albert
--- security.py
+++ security.py
@@ -12,22 +12,26 @@ def get_api_key(api_key_header: str = Security(api_key_header)) -> str:
     # ignore api key validation in development
     if ENV == "dev":
         return api_key_header
-
+    
     # check if api key is in the correct format
     if not api_key_header.startswith("Bearer "):
+        
         raise HTTPException(
             status_code=401,
             detail="Invalid or missing API Key",
         )
+    
+    api_key_header = api_key_header.replace("Bearer ", "")
 
     # open api_keys json file
     with open(API_KEYS_FILE, "r") as file:
         api_keys = json.load(file)
 
-    available_keys = [key.replace("Bearer: ", "") for title, key in api_keys.items()]
+    available_keys = [key.replace("Bearer ", "") for title, key in api_keys.items()]
 
     if api_key_header in available_keys:
         return api_key_header
+    
     raise HTTPException(
         status_code=401,
         detail="Invalid or missing API Key",
