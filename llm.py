d48ce60fe084ead05d0b25aaab77d0cfa6992977
dtrckd <dtrckd@gmail.com>
2024-07-04 23:15:57 +0200

    fix sampling params

--- llm.py
+++ llm.py
@@ -10,6 +10,7 @@ def generate(query, model_name=LLM_DEFAULT_MODEL):
     prompt = prompter.make_prompt(query=query)
 
     # Generate
+    sampling_params = prompter.sampling_params
     llm_client = LlmClient(model_name)
-    answer = llm_client.generate(prompt)
+    answer = llm_client.generate(prompt, **sampling_params)
     return answer.strip()
