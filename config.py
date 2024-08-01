59b32360fc424cd6920b09187436d69acf44c46d
leoguillaume <leo.guillaume@modernisation.gouv.fr>
2024-08-01 12:25:26 +0200

    fix: change default model

--- config.py
+++ config.py
@@ -18,23 +18,10 @@ CONTACT = {
 API_KEYS_FILE = os.environ.get("API_KEYS_FILE", "api_keys.example.json")
 API_KEYS_FILE = json.load(open(API_KEYS_FILE, "r"))
 API_KEYS = [key for title, key in API_KEYS_FILE.items()]
-
-# Env
 ENV = os.environ.get("ENV", "dev")
-assert ENV in [
-    "unittest",
-    "dev",
-    "staging",
-    "prod",
-], "wrong ENV value. Should be 'unittest', 'dev', 'staging' or 'prod'"
-
-# CORS
+assert ENV in ["unittest", "dev", "staging", "prod"], "wrong ENV value"
 BACKEND_CORS_ORIGINS = ["*"]
-
-# Redis
 REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.environ.get("REDIS_PORT", "6379"))
 REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", None)
-
-LLM_DEFAULT_MODEL = os.getenv("LLM_DEFAULT_MODEL", "AgentPublic/llama3-fabrique-texte")
-assert LLM_DEFAULT_MODEL in [m["model"] for m in LLM_TABLE], f"{LLM_DEFAULT_MODEL} should be in LLM_TABLE"  # fmt: off
+MODEL_NAME = os.getenv("MODEL_NAME", "AgentPublic/llama3-fabrique-texte")
--- llm.py
+++ llm.py
@@ -1,10 +1,10 @@
 from pyalbert.clients import LlmClient
 from pyalbert.prompt import get_prompter
 
-from config import LLM_DEFAULT_MODEL
+from config import MODEL_NAME
 
 
-def generate(query, model_name=LLM_DEFAULT_MODEL):
+def generate(query, model_name=MODEL_NAME):
     # Build prompt
     prompter = get_prompter(model_name, mode="simple")
     messages = prompter.make_prompt(query=query)
