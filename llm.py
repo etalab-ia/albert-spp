91325d06af3621a4a00d7f9ce6b1a275dc7fd3db
dtrckd <dtrckd@gmail.com>
2024-08-02 02:22:51 +0200

    fix: autonomous prompt engineering.

--- llm.py
+++ llm.py
@@ -1,12 +1,18 @@
+from pathlib import Path
+
 from pyalbert.clients import LlmClient
-from pyalbert.prompt import get_prompter
+from pyalbert.prompt import Prompter
 
-from config import MODEL_NAME
+from config import ALBERT_API_KEY, MODEL_NAME
 
 
-def generate(query, model_name=MODEL_NAME):
+def generate(query, model_name=MODEL_NAME, albert_api_key=ALBERT_API_KEY):
     # Build prompt
-    prompter = get_prompter(model_name, mode="simple")
+    prompter = Prompter(
+        config={"sampling_params": {"temperature": 0.25, "max_tokens": 4096}},
+        template=Path(__file__).resolve().parent / "templates/spp_fabrique_simple.jinja",
+        api_key=ALBERT_API_KEY,
+    )
     messages = prompter.make_prompt(query=query)
 
     # Generate
