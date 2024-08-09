a9caa56dc3f3b6bc2ae34619fb003da313d15add
dtrckd <dtrckd@gmail.com>
2024-08-09 12:43:47 +0200

    minor: change defaut model conf

--- config.py
+++ config.py
@@ -23,4 +23,4 @@ BACKEND_CORS_ORIGINS = ["*"]
 REDIS_HOST = os.environ.get("REDIS_HOST", "redis")
 REDIS_PORT = int(os.environ.get("REDIS_PORT", "6379"))
 REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", None)
-MODEL_NAME = os.getenv("MODEL_NAME", "AgentPublic/llama3-fabrique-texte")
+MODEL_NAME = os.getenv("MODEL_NAME", "AgentPublic/llama3-instruct-8b")
